b526519e4b08ceb0c679a1158b319fa9b190eedd
JamunaCerlin <jamunacerlin@gmail.com>
2025-05-23 12:28:43 +0530

    chromium launch

--- globalSetup.js/global-setup.js
+++ globalSetup.js/global-setup.js
@@ -1,9 +1,8 @@
 const { chromium } = require('@playwright/test');
 
-module.exports = async ({browser}) => {
-
+module.exports = async () => {
+  const browser = await chromium.launch();
   const context = await browser.newContext();
-
   const page = await context.newPage();
 
   await page.goto('https://rahulshettyacademy.com/client');
